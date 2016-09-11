c229a80c64fb470e59e4dd7448312a826d3e6021
others <2133123@qq.com>
2016-09-11 20:59:03 +0800

    add 7 files

---
+++ sqlTest.py
@@ -0,0 +1,6 @@
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# 创建对象的基类:
+Base = declarative_base()
